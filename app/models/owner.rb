c28cde579663d297e50720b5b2f7b484ab3c06b1
Your Name <abdulaiabubakar473@gmail..com>
2024-10-28 14:45:58 -0500

    Validation added to the owner

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,6 +1,7 @@
 class Owner < ApplicationRecord
   has_many :dogs
   has_many :breeds, through: :dogs
+  #validation Added to the owner
   validates :name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
