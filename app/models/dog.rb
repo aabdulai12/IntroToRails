f3935765cdbfc52b90e0dba5cec233c64258eb24
Your Name <abdulaiabubakar473@gmail..com>
2024-10-28 14:38:05 -0500

    Validation for dog models

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,6 +3,7 @@ class Dog < ApplicationRecord
   belongs_to :owner, optional: true  # Temporarily allow null owner_id for seeding
   has_many :vet_visits
 
+  # Validation for each
   validates :name, presence: true, length: { minimum: 2 }
   validates :age, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 20 }
 end
